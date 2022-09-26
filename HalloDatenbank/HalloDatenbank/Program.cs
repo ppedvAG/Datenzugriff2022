6295fc948dc98497b6b28339e6bfb89d189c9ea9
André <AndreR@ppedv.de>
2022-09-26 14:35:42 +0200

    Sql Query

--- HalloDatenbank/HalloDatenbank/Program.cs
+++ HalloDatenbank/HalloDatenbank/Program.cs
@@ -11,10 +11,47 @@ namespace HalloDatenbank
             string conString = "Server=(localdb)\\mssqllocaldb;Database=NORTHWND;Trusted_Connection=true;TrustServerCertificate=true;";
 
             SqlConnection con = new SqlConnection(conString);
-            con.Open();
 
-            Console.WriteLine("DB Verbindung wurde hergestellt");
-                       
+            try
+            {
+                con.Open();
+                Console.WriteLine("DB Verbindung wurde hergestellt");
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "SELECT COUNT(*) FROM Employees";
+                var count = cmd.ExecuteScalar();
+                Console.WriteLine($"Es wurde {count} Employees gefunden");
+
+                SqlCommand queryCmd = new SqlCommand();
+                queryCmd.Connection = con;
+                queryCmd.CommandText = "SELECT * FROM Employees";
+                SqlDataReader reader = queryCmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    //string vorName = reader.GetString(2);
+                    //string nachName = (string)reader["LastName"];
+                    //int id = reader.GetInt32(0);
+
+                    string nachName = reader.GetString(reader.GetOrdinal("LastName"));
+                    string vorName = reader.GetString(reader.GetOrdinal("FirstName"));
+                    int id = reader.GetInt32(reader.GetOrdinal("EmployeeId"));
+                    DateTime gebDatum = reader.GetDateTime(reader.GetOrdinal("BirthDate"));
+
+                    Console.WriteLine($"[{id}] {vorName} {nachName} ({gebDatum:d})");
+                }
+                reader.Close();
+                con.Close();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine($"DB-ERROR: {ex.Message} Code {ex.Number}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex.Message}");
+            }
+
         }
     }
 }
