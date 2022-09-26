7368201e2958da41e489173ddb7772c0c15ae2a3
André <AndreR@ppedv.de>
2022-09-26 12:37:44 +0200

    DB Connection

---
+++ HalloDatenbank/HalloDatenbank/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace HalloDatenbank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string conString = "Server=(localdb)\\mssqllocaldb;Database=NORTHWND;Trusted_Connection=true;TrustServerCertificate=true;";
+
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+
+            Console.WriteLine("DB Verbindung wurde hergestellt");
+                       
+        }
+    }
+}
