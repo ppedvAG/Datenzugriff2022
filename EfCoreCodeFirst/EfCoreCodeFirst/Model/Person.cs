178be4a34318f25c9ddd4fa74a7350c096ce46b2
André <AndreR@ppedv.de>
2022-09-28 12:25:56 +0200

    Model Config per DataAnnoations

--- EfCoreCodeFirst/EfCoreCodeFirst/Model/Person.cs
+++ EfCoreCodeFirst/EfCoreCodeFirst/Model/Person.cs
@@ -1,8 +1,16 @@
-﻿namespace EfCoreCodeFirst.Model
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EfCoreCodeFirst.Model
 {
+    [Table("Leute")]
     public abstract class Person
     {
-        public int Id { get; set; }
+        [Key]
+        public int EiDii { get; set; }
+        [Required]
+        [Column("VorUndNachname")]
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
         public DateTime GebDatum { get; set; }
     }
