d39e9af8ffda4b699f7b759b8766d9d34ece0d3f
André <AndreR@ppedv.de>
2022-09-28 11:56:56 +0200

    Demodaten

--- EfCoreCodeFirst/EfCoreCodeFirst/Form1.Designer.cs
+++ EfCoreCodeFirst/EfCoreCodeFirst/Form1.Designer.cs
@@ -71,6 +71,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Create Demo data";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // dataGridView1
             // 
--- EfCoreCodeFirst/EfCoreCodeFirst/Form1.cs
+++ EfCoreCodeFirst/EfCoreCodeFirst/Form1.cs
@@ -1,4 +1,5 @@
 using EfCoreCodeFirst.Data;
+using EfCoreCodeFirst.Model;
 
 namespace EfCoreCodeFirst
 {
@@ -16,6 +17,29 @@ namespace EfCoreCodeFirst
         private void ladenButton_Click(object sender, EventArgs e)
         {
             dataGridView1.DataSource = _context.Mitarbeiter.ToList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var abt1 = new Abteilung() { Bezeichnung = "Holz" };
+            var abt2 = new Abteilung() { Bezeichnung = "Steine" };
+
+            for (int i = 0; i < 100; i++)
+            {
+                var m = new Mitarbeiter()
+                {
+                    Name = $"Fred {i:000}",
+                    Beruf = "Macht dinge",
+                    GebDatum = DateTime.Now.AddYears(-40).AddDays(i * 17)
+                };
+                if (i % 2 == 0)
+                    m.Abteilungen.Add(abt1);
+                if (i % 3 == 0)
+                    m.Abteilungen.Add(abt2);
+
+                _context.Mitarbeiter.Add(m);
+            }
+            _context.SaveChanges();
         }
     }
 }
