35ee3ef7a7b4c1d4f903e22035178585bf4ee033
André <AndreR@ppedv.de>
2022-09-29 16:38:29 +0200

    Tests mit xunit

---
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -0,0 +1,56 @@
+namespace Calculator.Tests
+{
+    public class CalcTests
+    {
+        [Fact]
+        public void Sum_2_and_3_results_5()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(2, 3);
+
+            //Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Sum_0_and_0_results_0()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(0, 0);
+
+            //Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Sum_MAX_and_1_throws_OverflowEx()
+        {
+            var calc = new Calc();
+
+            Assert.Throws<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+        }
+
+        [Theory]
+        [InlineData(0, 0, 0)]
+        [InlineData(2, 5, 7)]
+        [InlineData(-3, -5, -8)]
+        [InlineData(-3, 5, 2)]
+        public void Sum(int a, int b, int exp)
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act
+            var result = calc.Sum(a, b);
+
+            //Assert
+            Assert.Equal(exp, result);
+        }
+    }
+}
